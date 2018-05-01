b3764889aa1d6a5c2d2db3c2544c32e7bac1380d
Spik3Scarlett <gitarsitar@gmail.com>
2018-05-01 16:20:14 +0200

    Finish 9 advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,12 @@ class SessionsController < ApplicationController
    user = User.find_by(email: params[:session][:email].downcase)
    if user && user.authenticate(params[:session][:password])
      log_in user
+     params[:session][:remember_me] == '1' ? remember(user) : forget(user) # jesli checkbox remember zaznaczony
+     # nastepuje remember, czyli odwoluje sie do metody remember z sessions helpera - tworzy
+     # token i jego zahaszowana wersje zapisuje w bazie oraz od razu zapisuje cookiesy
+     # jesli nie jest zaznaczony, nastepuje forget user
+
+
      redirect_to user
    else
     flash.now[:danger] = 'Zły login lub hasło'
@@ -14,7 +20,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in? # powoduje wylogowanie tylko jesli user jest zalogowany.
+    # jesli wiec ktos sie wyloguje w jednym oknie to klikniecie wyloguj w innym oknie nie zwroci bledu
+    # tylko spowoduje przejscie do kolejnej linijki kodu
     redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,16 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any).
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id]) # jezeli da sie przypisac user_id do session[:user_id] (tylko jesli to ostatnie ISTNIEJE!)
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+    #nie ma else, bo inaczej current_user nie istnieje - nikt nie jest zalogowany
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -14,7 +23,23 @@ module SessionsHelper
   end
 
   def log_out
-     session.delete(:user_id)
+     forget(current_user) #odwoluje sie do metody z session helper (patrz dalej)
+     session.delete(:user_id) # likwiduje tymczasowe cookie sessji
      @current_user = nil
    end
+
+   def remember(user) # pierwsza linijka tworzy token i zapisuje jego zahaszowana wersje w bazie,
+     # druga i trzecia zapisuja zaszyfrowane cookie ID i TOKENA w przegladarce
+     user.remember
+     cookies.permanent.signed[:user_id] = user.id
+     cookies.permanent[:remember_token] = user.remember_token
+   end
+
+   # Forgets a persistent session. Pierwsza linijka likwiduje z modelu zaszyfrowany token (remember_digest)
+   def forget(user)
+     user.forget
+     cookies.delete(:user_id)
+     cookies.delete(:remember_token)
+   end
+
 end
